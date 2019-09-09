b4bc1e9c2c7fe68cc503393eda9d271e9e439cfb
Mario Lopriore <mariojames211@gmail.com>
2019-09-09 16:14:36 -0700

    Use toggleClass({}) not css({}).

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@
 jQuery(window).scroll(function() {    // this will work when your window scrolled.
   var height = jQuery(window).scrollTop();  //getting the scrolling height of window
   if(height  > 100) {
-    jQuery(".main-navigation").css({"position": "fixed"});
+    jQuery(".main-navigation").css({
+      "position": "fixed",
+      "background": "#fff",
+      "padding": "10px 0",
+      "margin-top": "0",
+      "top": "0",
+      "z-index": "999"
+  });
   } else{
-    jQuery(".main-navigation").css({"position": "relative"});
+    jQuery(".main-navigation").css({
+      "position": "relative",
+      "background": "transparent"
+    });
   }
 });
